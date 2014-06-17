d06d711eba11a249f3b2f194dc5255f0819354cc
jonathan.eidelman@mail.mcgill.ca <jonathan.eidelman@mail.mcgill.ca>
2014-06-17 15:58:19 -0500

    Added forms

--- index.php
+++ index.php
@@ -2,15 +2,28 @@
   <head>
     <title> Application for carrier updates </title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="applicationMain.css">
   </head>
   
   <body>
-    <div id="displayFrame">
-      <h3> Displays go here! </h3>
+    <div id="main">
+      <div id="leftFrame">
+	
+	<form name="carrierId">	
+	  Please insert Carrier Tracking ID: <input type="text" name="firstname">
+	</form>
+	<a href="#"><div id="submitID"> Search DB </div></a>
+      </div>
+      
+      <div id="middleFrame">
+	<h3> Inputs go here! </h3>
+      </div>
+      
+      <div id="rightFrame">
+	<a href="#"><div id="submitChanges"> Submit Changes </div></a>
+      </div>
     </div>
 
-    <div id="inputFrame">
-      <h3> Inputs go here! </h3>
-    </div>
   </body>
+
 </html>

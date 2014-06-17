0fe0338a3cf04f5226f462b51beadae233bab27d
jonathan.eidelman@mail.mcgill.ca <jonathan.eidelman@mail.mcgill.ca>
2014-06-17 15:04:42 -0500

    Index page for our website

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title> Application for carrier updates </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  </head>
+  
+  <body>
+    <div id="displayFrame">
+      <h3> Displays go here! </h3>
+    </div>
+
+    <div id="inputFrame">
+      <h3> Inputs go here! </h3>
+    </div>
+  </body>
+</html>

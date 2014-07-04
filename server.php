230d42c4f7365d1504eb587d46ecc68fd5386960
Jonathan Eidelman <jonathaneidelman@web112038.wireless.mcgill.ca>
2014-07-04 10:01:26 -0400

    Added small server

--- server.php
+++ server.php
@@ -16,7 +16,8 @@ for ($i=0; $i<100; $i++) {
             "type" => "type".$k,
             "start" => "5-1-1981",
             "end" => "9-1-1999",
-            "term" => ""
+            "term" => "",
+            "note" => "Owner".$k." is the greatest!"
         );
         array_push($provList, $newOwner);
         
@@ -25,7 +26,7 @@ for ($i=0; $i<100; $i++) {
 
     
     $newEntry = array(
-        "description" => "This is a textile. It is the greatest textile ever created. I love it with all my heart. I love it so much I want to eat it and then clothe myself only in it for the rest of my life. It is truly the most lovely piece of fabric every imagined by mankind. I mean humankind! I apologize for my gender insensitivty. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "provenance" => $provList,
         "tags" => $tagList
     ); 
